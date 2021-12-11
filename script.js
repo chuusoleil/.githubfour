01d16ea04ed36be2d5dda70b8cef63ec3662d518
chuusoleil <93917295+chuusoleil@users.noreply.github.com>
2021-12-11 21:08:03 +0900

    Update script.js

--- script.js
+++ script.js
@@ -12,12 +12,34 @@ $(function(){
         $('#header').removeClass('open');
     });
 
+    $('#navi a').click(function(){
+        $('#header').removeClass('open');
+    });
+
+
+    
+
 
     $('.pickup-items').slick({
         arrows: false,
         centerMode: true,
         centerPadding: '100px',
         slidesToShow: 3,
+    });
+
+
+
+    $(window).scroll(function(){
+        $('.fadein').each(function(){
+            let scroll = $(window).scrollTop();
+            let target = $(this).offset().top;
+            let windowHeight = $(window).height();
+
+            if (scroll > target - windowHeight + 200){
+                $(this).css('opacity','1');
+                $(this).css('transform','translateY(0)');
+            }
+        });
     });
 
 });
